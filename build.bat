f7a2e87015f07919009b20bb40e4d490ea7b3294
Costa Jorge <jorge.costa@tekla.com>
2016-03-19 18:40:35 +0200

    use last api

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo on
+nuget restore
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
 msbuild BuildPlugin.msbuild /p:VisualStudioVersion=14.0 /p:EndVSQFile=ProjectDuplicationTracker.VSQ /p:AssemblyPatcherTaskOn=true /p:SkipCopy=No > buildlog2015.txt
